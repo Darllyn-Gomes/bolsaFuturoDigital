3e8f153bfa45aad5815edbca375d24465aa9ed23
Darllyn <darllyngomes@gmail.com>
2025-09-04 17:04:46 -0300

    Aula08, dados, MER

---
+++ aula08/receitaBolo.ts
@@ -0,0 +1,32 @@
+class ReceitaDeBolo {
+  public nome: string;
+  public tempoPreparoMin: number;
+  public ingredientes: string[];
+  public modoPreparo: string;
+  public rendimento: number;
+  public sabor: string;
+
+  listarIngredientes() {
+    
+  }
+  prepararMassa() {
+
+  }
+  
+
+  assar() {
+    
+  }
+
+  servir() {
+    
+  }
+
+  decorar() {
+    
+  }
+
+  verificarPronto() {
+    
+  }
+}
